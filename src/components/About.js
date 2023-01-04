f5d071fe4a1bfb34841661949388efabcf2bbb2b
Averi <averi.sherman@gmail.com>
2023-01-04 11:09:33 -0500

    paragragh on myself

--- src/components/About.js
+++ src/components/About.js
@@ -11,9 +11,8 @@ function About() {
                 <div className="nine columns main-col">
                     <h2>About Me</h2>
                     <p>
-                        Hi! I am a recent graduate of the UCLA Coding Bootcamp. placeholder text
-                        placeholder bio text
-                        placehilder bio text
+                        Hi! I am a recent graduate of the UCLA Coding Bootcamp for Fullstack Web Developer.  I come from a background in Fire Science from my previous career and am excited to continue to learn and develop my new coding skills. My favorite part of coding is collaborating with others on projects that are functional, useful, and fun.
+                        In my free time I enjoy mountain biking, puzzles, and being with friends and family. 
                     </p>
                     <div className="nine columns main-col">
                         <h2>Technical Skills</h2>
